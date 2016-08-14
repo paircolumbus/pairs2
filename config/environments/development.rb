5a77a4a0b975fe6b0af639753b63c64c859ef048
Erin Anthony <erinmadisonanthony@gmail.com>
2016-08-14 22:55:21 +0000

    adjust localhost address for ActionMailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   #Set up ActionMailer default
-  config.action_mailer.default_url_options = { host: 'localhost:3030' }
+  config.action_mailer.default_url_options = { host: 'localhost:3333' }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
