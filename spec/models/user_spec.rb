e119a0e70fdfe04af6d3a22612864ec81663a725
vpappu <vpappu@covermymeds.com>
2016-08-15 18:53:31 -0400

    add basic user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory"
+  it "is invalid without a first name"
+  it "is invalid without a last name"
+  it "is invalid without an email"
+  it "is invalid without a username"
 end
