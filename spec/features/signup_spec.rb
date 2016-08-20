50a3469825ff89acf563d948c74af07ff356f98c
tkendalls <director@mathplusacademy.com>
2016-08-20 12:12:33 -0400

    Update sign up form with first and last name fields. Adjust sign_up spec to include first and last name.

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.feature "Signup", type: feature do
   scenario "brand new user signs up for an account" do
     visit "/sign_up"
+    fill_in "First name", with: "Pat"
+    fill_in "Last name", with: "Doe"
     fill_in "Email", with: 'abc@def.com'
     fill_in "Password", with: 'abcdef1'
     fill_in "Password confirmation", with: 'abcdef1'
