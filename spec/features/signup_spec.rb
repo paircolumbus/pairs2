3befde6876fab6985fb5f7d1ff59df041fad63bf
tkendalls <director@mathplusacademy.com>
2016-08-20 12:24:34 -0400

    Add skill level and username to forms, controller, and spec.

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.feature "Signup", type: feature do
   scenario "brand new user signs up for an account" do
     visit "/sign_up"
-    fill_in "First name", with: "Pat"
-    fill_in "Last name", with: "Doe"
+    fill_in "First name", with: 'Pat'
+    fill_in "Last name", with: 'Doe'
+    fill_in "Username", with: 'pat_doe'
     fill_in "Email", with: 'abc@def.com'
     fill_in "Password", with: 'abcdef1'
     fill_in "Password confirmation", with: 'abcdef1'
+    select "Beginner", from: 'user[skill_level]'
     click_on "Sign up"
 
     expect(page.status_code).to eq 200
