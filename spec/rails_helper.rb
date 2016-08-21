90ef922a5bff973915a9bdc0cba96ea1592b24b6
tkendalls <director@mathplusacademy.com>
2016-08-20 20:27:49 -0400

    Add components for Devise/rspec testing setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require 'support/factories'
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -58,4 +59,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Capybara::DSL
+
+  # Setup reqired for devise
+   config.include Devise::Test::ControllerHelpers, :type => :controller
 end
