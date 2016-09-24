10e1f9cffc55e84fd9ec8ebd114bc530c04e4ffd
Jay Bobo <jbobo@covermymeds.com>
2016-09-24 10:49:39 -0400

    create pair associations

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,2 +1,5 @@
 class Pair < ApplicationRecord
+  belongs_to :event
+  belongs_to :driver, class_name: 'Reservation', foreign_key: 'person_1'
+  belongs_to :navigator, class_name: 'Reservation', foreign_key: 'person_2'
 end
--- spec/models/pair_spec.rb
+++ spec/models/pair_spec.rb
@@ -1,5 +1,7 @@
 require "rails_helper"
 
-describe Pair do
-
+RSpec.describe Pair, type: :model do
+  it { is_expected.to belong_to(:event) }
+  it { is_expected.to belong_to(:driver) }
+  it { is_expected.to belong_to(:navigator) }
 end
