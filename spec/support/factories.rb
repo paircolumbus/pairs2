3314a0ed811a311f510225edac0d4c7e0d5a7067
paradime <brw4824@rit.edu>
2016-10-21 17:55:04 -0400

    add bare minimum to setup factory girl reservation

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -9,4 +9,31 @@ FactoryGirl.define do
     u.password    { "abcdef123" }
     u.skill_level { Faker::Number.between(1, 3) }
   end
+
+  factory :pair do |p|
+    p.status { 0 }
+  end
+
+  factory :reservation do |r|
+  end
+
+  factory :reservation_status do |rs|
+  end
+
+  factory :skill do |s|
+    s.name { 'rails' }
+  end
+
+  factory :event do |e|
+    e.name { 'halloween' }
+    e.date { DateTime.now }
+    e.start_time { Time.now }
+    e.end_time { Time.now + 1.second }
+    e.status { 1 }
+  end
+  
+  factory :organization do |o|
+    o.name { 'pair columbus' }
+    o.description { 'pair columbus is awesome' }
+  end
 end
