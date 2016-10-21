0dc857307b4304d4058cef436198abd2c171fa05
tkendalls <director@mathplusacademy.com>
2016-10-20 21:25:25 -0400

    Add new org page

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,9 +1,12 @@
 class OrganizationsController < ApplicationController
+  respond_to :html
+
   def index
     @organizations = Organization.all
   end
 
   def new
+    @organization = Organization.new
   end
 
   def create
@@ -18,8 +21,9 @@ class OrganizationsController < ApplicationController
   def destroy
   end
 
-   private
+    private
 
-   def organization_params
-   end
+      def organization_params
+        params.require(:organization).permit(:name, :description, :admin_id)
+      end
 end
